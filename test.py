b74de562112e188e2ced51814dc3e2742981c836
Cameron Bennett <cameron.bennett@queensu.ca>
2023-01-02 17:36:57 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from ctypes import cast, POINTER
+import pyaudio
+import numpy as np
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+current = volume.GetMasterVolumeLevel()
+print(current)
